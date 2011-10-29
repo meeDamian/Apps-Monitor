2c5eba576c7eff1bea3f73bc1c2b247bebacb337
Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>
2011-10-29 18:07:23 +0200

    preparation for saving to localStorage

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ $(function(){
     $('#new_trigger').click(function(){
         $('#new_form').toggle();
     });
+    
+    $('#new_form').submit(function(){
+        
+        // save data
+        tmp = new Array();
+        tmp['name'] = $('#new_name').val();
+        tmp['type'] = $('#new_type').val();
+        tmp['url']  = $('#new_url').val();
+        
+        if(!localStorage.apps) localStorage.apps = JSON.stringify(tmp);
+        else {
+            tmpStorage = JSON.parse(localStorage.apps);
+        }
+        
+        // clear form
+        $('#new_name,#new_url,#new_type').val('');
+        return false;
+    });
+
+    if(!localStorage.apps) localStorage.apps = Array();
+    console.log(localStorage.apps);
 });
