6435daa4cb20c210acd8e66896bedf7a6c75c60f
Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>
2012-04-26 20:38:33 +0200

    Extension in it's earlier form finished. For now only returns state upon click

--- main.js
+++ main.js
@@ -1,63 +1,26 @@
-// implement JSON.stringify serialization
-// from: http://stackoverflow.com/questions/3593046/jquery-json-to-string
-JSON.stringify = JSON.stringify || function (obj) {
-    var t = typeof (obj);
-    if (t != "object" || obj === null) {
-        // simple data type
-        if (t == "string") obj = '"'+obj+'"';
-        return String(obj);
-    } else {
-        // recurse array or object
-        var n, v, json = [], arr = (obj && obj.constructor == Array);
-        for (n in obj) {
-            v = obj[n]; t = typeof(v);
-            if (t == "string") v = '"'+v+'"';
-            else if (t == "object" && v !== null) v = JSON.stringify(v);
-            json.push((arr ? "" : '"' + n + '":') + String(v));
-        }
-        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-    }
-};
-
 $(function(){
-    $('#new_trigger').click(function(){
-        $('#new_form').slideToggle();
-    });
-    
-    $('#new_form').submit(function(){
-        
-        // save data
-        tmp = new Object();
-        tmp.name = $('#new_name').val();
-        tmp.type = $('#new_type').val();
-        tmp.url  = $('#new_url').val();
-        
-        if(localStorage.getItem('apps') !== null) {
-            var added = jQuery.parseJSON(localStorage.getItem('apps'));
-            console.log(added);
-            console.log(tmp);
-            
+    $.get('http://facebook.webtop.pl/templates/tests/db_test.php',{level:3},function(data){
+        if(data.error===undefined) {
+            $('#auth').submit(function(){
+                $('#loader').show();
+                $.post('http://facebook.webtop.pl/templates/tests/jx/do.php',$(this).serialize(),function(data){
+                    $('#loader').hide();
+                    if(!data.success) $('#quickInfo').show().children('span').text(data.cause);
+                    else { }
+                });
+                return false;
+            });
+        } else {
+            $('#loader,#auth').remove();
+            if(data.error) $('#quickInfo span').addClass('error').text('Wystąpił błąd!');
+            else $('#quickInfo span').addClass('success').text('Wszystko jest OK. ');
+
+            $('#quickInfo a').text('więcej informacji').attr('href',data.url)
+                .click(function(){
+                    window.open($(this).attr('href'));
+                });
+            $('#quickInfo').show();
         }
 
-        // stringize
-        var result = JSON.stringify(tmp);
-        localStorage.setItem('apps',result);
-        
-        // add new position in apps list
-        $('#t_added .app').clone().appendTo($('#apps_list'));
-        $('#apps_list:last').find('.app_name').html(tmp.name);
-        $('#apps_list:last').find('.app_status').html("OK");
-        $('#apps_added').show();
-        
-        // clear and hide form
-        $('#new_name,#new_url,#new_type').val('');
-        $('#new_form').slideUp();
-        return false;
     });
-    
-    
-    if(localStorage.getItem('apps') !== null) {
-        console.log(localStorage.getItem('apps'));
-        $('#apps_added').show();
-    }
 });
